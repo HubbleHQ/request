97da1ff1d2001e8a08845d666e9da6d7a7d0c11d
Simon Andrews <simon@hubblehq.com>
2020-04-30 15:48:56 +0100

    Add Jest

---
+++ .jest/setup.js
@@ -0,0 +1,4 @@
+import "regenerator-runtime/runtime";
+
+// for mocking fetch requests
+global.fetch = require('jest-fetch-mock');
--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,17 @@
 module.exports = function (api) {
+  const isTest = api.env('test');
   api.cache(true);
 
-  const presets = [ require('@babel/preset-env') ];
+  const presets = [
+    [
+      require('@babel/preset-env'),
+      ...(isTest ? [{ targets: { node: 'current' } }] : []),
+    ],
+  ];
   const plugins = [];
 
   return {
     presets,
-    plugins
+    plugins,
   };
-}
+};
